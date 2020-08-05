3718d9ff32507bb0cd6e50018b9107ba99023077
Pytode <49377897+ongjiachun@users.noreply.github.com>
2020-08-05 14:46:58 +0800

    Changed the color scheme and added a new tab for patients ("Diseases")
    Changed the color scheme to suit a medical website more (white rather than black themed).
    
    Added a new navigation tab called "Diseases" for account_type = "Patient". In this tab, all diseases and their information will be shown.

--- ThyroidSpot/js/test.js
+++ ThyroidSpot/js/test.js
@@ -1,7 +1,7 @@
 // Test script to test the different access control in the navigation bar.
 
 // var user_logged_in = "n"
-var user_logged_in = "n"
+var user_logged_in = "y"
 
 var user_account_type = "patient"
 // var user_account_type = "clinician"
