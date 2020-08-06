fe7c35e0eaa7f05ed382f49fff292e2ebd5f5457
Ksnaeh <65495468+Ksnaeh@users.noreply.github.com>
2020-08-06 23:09:13 +0800

    Adding backend

---
+++ ThyroidSpotAppServices/ThyroidSpotAppServices/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ThyroidSpotAppServices.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
