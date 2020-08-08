53a4c3b6dec8b286af8030b0a4312d910e49a397
Ksnaeh <65495468+Ksnaeh@users.noreply.github.com>
2020-08-08 16:33:23 +0800

    improvements on disease page

--- ThyroidSpot/js/disease-function.js
+++ ThyroidSpot/js/disease-function.js
@@ -5,6 +5,7 @@ var currentDiseaseID; //this variable will contain the disease name that's selec
 
 //This function will consume the disease GET API
 function getAllDiseaseInfo() {
+    console.log("retrieving all disease info...")
     $.ajax({
         type: 'GET',
         url: diseaseURI,
@@ -13,13 +14,20 @@ function getAllDiseaseInfo() {
         success: function (data) {
             //put json data into bookArray
             diseaseInfoArray = data;
+            console.log(diseaseInfoArray)
             //clear the tbody of the table so that it will be refreshed everytime
             $('#diseaseCard').html('');
             //Iterate through the diseaseInfoArray to generate rows to populate the table
-            for (i = 0; i < disinfoArray.length; i++) {
-                coverImage = "<img style='width:100px' src='data:image/jpeg;base64," + disinfoArray[i].thumbnail + "'/>";
-                $('#diseaseCard').append("<div onclick='document.getElementById('diseaseModal').style.display='block'' class = 'centerText'><img  src='" +
-                    coverImage + "><div class='centerTitle'><b>" + diseaseInfoArray[i].disease + "</b></div></div>");
+            for (i = 0; i < diseaseInfoArray.length; i++) {
+                coverImage = "<img style='width:100px' src='data:image/jpeg;base64," + diseaseInfoArray[i].thumbnail + "'/>";
+
+                //use sample image for now:
+                coverImage= "<img src='https://www.mei.edu/sites/default/files/2019-01/Virus.jpg'>"
+                // viewmorebutton = "document.getElementById('diseaseModal').style.display='block'"
+                // $('#diseaseCard').append("<div onclick="+viewmorebutton+"  class = 'centerText'>" +
+                //     coverImage + "<div class='centerTitle'><b>" + diseaseInfoArray[i].disease + "</b></div></div>");
+                $('#diseaseCard').append("<div id='viewmore' num="+i+"  class = 'centerText'>" +
+                    coverImage + "<div class='centerTitle'><b>" + diseaseInfoArray[i].disease + "</b></div></div>");
             }
         }
     });
@@ -28,6 +36,7 @@ function getAllDiseaseInfo() {
 
 //This function will get one disease info
 function getOneDiseaseInfo(id) {
+    id
     console.log(id);
     $.ajax({
         type: 'GET',
@@ -38,16 +47,17 @@ function getOneDiseaseInfo(id) {
             //put json data into bookArray
             diseaseInfoArray = data;
             //Iterate through the bookArray to generate rows to populate the table
-            for (i = 0; i < disinfoArray.length; i++) {
-                if (id == disinfoArray[i].disease) {
-                    localStorage.setItem("diseasename", id);
+            
+            for (i = 0; i < diseaseInfoArray.length; i++) {
+                if (id == i) {
+                    localStorage.setItem("diseasename", diseaseInfoArray[i].disease);
                     //generate the buttons html with information hidden into its attributes (index)
                     //num attribute is used to store the array index so that in can be used later to retrieve information from the bookArray
                     //coverImage = "<img style='width:100px' src='data:image/jpeg;base64," + disinfoArray[i].thumbnail + "'/>";
                     $('#diseaseName').text(diseaseInfoArray[i].disease)
                     $('#diseaseShortDescription').text(diseaseInfoArray[i].description)
                     $('#diseaseContent').text(diseaseInfoArray[i].disease_content)
-                    $('#diseaseSymtoms').text(diseaseInfoArray[i].symtom)
+                    $('#diseaseSymptoms').text(diseaseInfoArray[i].symptom)
                     $('#diseaseCause').text(diseaseInfoArray[i].cause)
                     $('#diseaseTreatment').text(diseaseInfoArray[i].treatment)
                 }
@@ -55,3 +65,12 @@ function getOneDiseaseInfo(id) {
         }
     });
 }
+
+$(document).on("click", "#viewmore", function () {
+    currentDiseaseID = $(this).attr('num');
+    console.log(currentDiseaseID);
+    getOneDiseaseInfo(currentDiseaseID);
+    document.getElementById('diseaseModal').style.display='block'
+});
+
+getAllDiseaseInfo();
