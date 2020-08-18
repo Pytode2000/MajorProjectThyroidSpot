fa474bdbca58f5dbb2f027257dcb39713d427c8a
FishThatCodes <yukailong2667@aliyun.com@users.noreply.github.com>
2020-08-18 23:00:24 +0800

    min
    min

--- ThyroidSpot/js/doctor-functions.js
+++ ThyroidSpot/js/doctor-functions.js
@@ -10,7 +10,26 @@ var valueFT4;
 var valueTSH;
 var latestUpdate;
 
-//This function will consume the disease GET API
+
+function getPatientName(userId) {
+    console.log("retrieving patient name...")
+    $.ajax({
+        aSync: false,
+        type: 'GET',
+        url: userURI,
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            userInfoArray = data;
+            for(i = 0; i < userInfoArray.length; i++) {
+                if (userId == userInfoArray[i].user_id) {
+                    patientName = userInfoArray[i].full_name;
+                }
+            }
+        }
+    });
+}
+
 function getAllPatientInfo() {
     console.log("retrieving all patient infomation...")
     $.ajax({
@@ -19,17 +38,14 @@ function getAllPatientInfo() {
         dataType: 'json',
         contentType: 'application/json',
         success: function (data) {
-            //put json data into bookArray
             patientInfoArray = data;
             console.log(patientInfoArray)
-            //clear the tbody of the table so that it will be refreshed everytime
             $('#patientCard').html('');
-            //Iterate through the diseaseInfoArray to generate rows to populate the table
             for (i = 0; i < patientInfoArray.length; i++) {
                 getPatientName(patientInfoArray[i].user_id)
-                patientReportDetails = getPatientReport(patientInfoArray[i].patient_id)
-                $('#patientCard').append("<article> <div class='text'><h3>"+patientName+"</h3><p>"+patientReportDetails.latestUpdate+"</p>"+
-                  "<p><b>Latest Update</b></p><p>FT4: "+patientReportDetails.valueFT4+"</p><p>TSH: "+patientReportDetails.valueTSH+"</p>"+
+                getPatientReport(patientInfoArray[i].patient_id)           
+                $('#patientCard').append("<article> <div class='text'><h3>"+patientName+"</h3><p>x</p>"+
+                  "<p><b>Latest Update</b></p><p>FT4:x</p><p>TSH: x</p>"+
                   "<a class='button' href='patient-info-doctor.html' >More Details</a>"+
                   "<a class='button' href='#'>Graph</a></div></article>");
             }
@@ -38,45 +54,30 @@ function getAllPatientInfo() {
 }
 
 
-//This function will get the full name of the patient
-function getPatientName(userId) {
-    console.log("retrieving patient name...")
-    $.ajax({
-        type: 'GET',
-        url: userURI,
-        dataType: 'json',
-        contentType: 'application/json',
-        success: function (data) {
-            //put json data into bookArray
-            userInfoArray = data;
-            for(i = 0; i < userInfoArray.length; i++) {
-                if (userId == userInfoArray[i].user_id) {
-                    return patientName = userInfoArray[i].full_name
-                }
-            }
-        }
-    });
-}
 
 //This function will get the full name of the patient
 function getPatientReport(patientId) {
     console.log("Retrieving Patient Report...")
     $.ajax({
+        aSync: false,
         type: 'GET',
         url: reportURI,
         dataType: 'json',
         contentType: 'application/json',
         success: function (data) {
             reportArray = data;
-            for(i = 0; i < reportArray.length; i++) {
+             for(i = 0; i < reportArray.length; i++) {
                 if (patientId == reportArray[i].patient_id) {
-                    return{
-                        valueFT4 : reportArray[i].FT4,
-                        valueTSH : reportArray[i].TSH,
-                        latestUpdate : reportArray[i].timestamp
-                    } ;
+                        valueFT4 = reportArray[i].FT4,
+                        valueTSH = reportArray[i].TSH,
+                        latestUpdate = reportArray[i].timestamp
+                    
                 }
             }
         }
     });
 }
+
+$(window).on('load', function() {
+    getAllPatientInfo();
+   });
