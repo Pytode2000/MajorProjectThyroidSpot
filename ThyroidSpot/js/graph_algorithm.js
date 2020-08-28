e5109a669e1a2061cc776c077bd2ad083ca28f78
Ksnaeh <65495468+Ksnaeh@users.noreply.github.com>
2020-08-28 20:43:00 +0800

    Graph algorithm js

---
+++ ThyroidSpot/js/graph_algorithm.js
@@ -0,0 +1,25 @@
+//THIS WILL BE WHERE THE GRAPH FUNCTION COMES
+
+//TODO: link chart.js with this js
+
+
+
+//TODO: bring in FT4 and TSH arrays from patient-function and patient-info-function
+
+
+
+//TODO: set up algorithm
+
+
+
+
+//TODO: getting outlier data
+
+
+
+//TODO: displaying calculated chart
+
+
+
+
+//TODO: export calculations to CSV
