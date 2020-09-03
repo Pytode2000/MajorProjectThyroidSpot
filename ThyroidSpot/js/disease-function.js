06c9acd3792f8c78ee22a91c400536b91296114a
Pytode <49377897+ongjiachun@users.noreply.github.com>
2020-09-03 12:20:46 +0800

    Minor update.

--- ThyroidSpot/js/disease-function.js
+++ ThyroidSpot/js/disease-function.js
@@ -32,7 +32,7 @@ function viewDiagnosisDisease() {
 
         setTimeout(function () {
             sessionStorage.removeItem("view_disease");
-        }, 5000);
+        }, 2000);
 
     }
     else {
