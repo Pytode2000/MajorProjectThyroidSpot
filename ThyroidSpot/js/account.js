c84d33d67a9cb08d9c30fe9dea3e3f313b5c4089
Pytode <49377897+ongjiachun@users.noreply.github.com>
2020-09-21 15:02:47 +0800

    Cleaned and added comments (mostly in JS). Still not done.

--- ThyroidSpot/js/account.js
+++ ThyroidSpot/js/account.js
@@ -1,9 +1,10 @@
+// URI to manage API.
 var userURI = "https://localhost:44395/api/User";
 var patientInfoURI = "https://localhost:44395/api/patientinfo";
+var diagnosisURI = "https://localhost:44395/api/diagnosis";
 
 
 /* Toggle between Login and Register Tab */
-
 // GET DOM
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
@@ -29,8 +30,7 @@ function toggleLogin() {
 }
 
 
-/* Firebase Login and Register function */
-// NOTE: FORM CONTROL NOT IMPLEMENTED YET.
+// Login function. Takes in email and password.
 function login() {
     const txtEmail = document.getElementById("loginEmail");
     const txtPassword = document.getElementById("loginPassword");
@@ -41,33 +41,28 @@ function login() {
     const promise = auth.signInWithEmailAndPassword(email, pass);
 
     promise.then(firebaseUser => {
-
-        //DO NOT REMOVE: need to use it to create patient report in case not created on sign in
-        // var s = sessionStorage.setItem("firebaseUID", firebaseUser.uid)
-
         setTimeout(function () {
             window.location.href = "profile.html";
         }, 3000);
 
     })
+    // Catch errors e.g. email not found or wrong password.
     promise.catch(e => {
         console.log(e.message);
-        // document.getElementById("modalId").innerHTML = currentSelectedUserArray.id;
         const loginAlert = document.getElementById("loginAlert");
 
         loginAlert.classList.remove("hide");
         loginAlert.innerHTML = e.message;
     });
 }
-
+// Register function. Takes in email, full name, password, retype password, NRIC, birthdate, diagnosis (or diagnoses), gender and blood type.
 function register() {
     const txtEmail = document.getElementById("registerEmail");
     const txtPassword = document.getElementById("registerPassword");
     const txtRePassword = document.getElementById("registerRetypePassword");
 
+    // Checks if the value of password and retype is the same.
     if (txtPassword.value === txtRePassword.value) {
-
-
         const email = txtEmail.value;
         const pass = txtPassword.value;
         const auth = firebase.auth();
@@ -77,8 +72,9 @@ function register() {
             const userUid = user.uid;
             console.log(userUid);
 
-            // Creates an instance (row) in the "user" table.
-            var userInstance = { user_id: userUid, full_name: $('#registerFullName').val(), account_type: "patient" }; // Register function only creates "patient" accounts.
+            // Creates an instance (row) in the "user" table. Account type is fixed as "patient". Admin and clinician accounts are to be created by admins
+            // in the admin's manage page. User instance's foreign key "user_id" is the user's firebase UID.
+            var userInstance = { user_id: userUid, full_name: $('#registerFullName').val(), account_type: "patient" };
             console.log(userInstance)
             $.ajax({
                 type: 'POST',
@@ -88,14 +84,11 @@ function register() {
                 contentType: 'application/json',
                 success: function (data) {
                     console.log("User instance created.")
-                    // $('#registerFullName').val('')
                 }
             });
 
-            // Creates an instance (row) in the "patient_information" table.
-            // var diagnosis = document.getElementById("registerDiagnosis");
-            // var diagnosisChosen = diagnosis.options[diagnosis.selectedIndex].text;
-
+            /* For "patient_information" instace.*/
+            // Obtain the selected gender radio button's value.
             var genderChosen;
             if (document.getElementById('genderMaleRadio').checked) {
                 genderChosen = "Male"
@@ -104,9 +97,12 @@ function register() {
                 genderChosen = "Female"
             }
 
+            // Obtain the selected blood type select menu value.
             var bloodType = document.getElementById("registerBloodType");
             var bloodTypeChosen = bloodType.options[bloodType.selectedIndex].text;
 
+            // Creates an instance (row) in the "patient_information" table. Fields in this row: NRIC, birthdate, gender and blood type. The foregin key is
+            // the user's firebase UID. "doctor_id" is set as empty string by default (will be updated after a clinician "adopts" this patient).
             var patientInfoInstance = {
                 user_id: userUid, ic_number: $('#registerId').val(),
                 date_of_birth: $('#registerBirthdate').val(), gender: genderChosen, blood_type: bloodTypeChosen, timestamp: "-", doctor_id: ""
@@ -122,7 +118,6 @@ function register() {
                 success: function (data) {
                     console.log("Patient's Data: " + data)
 
-
                     // GET patient'id using firebase UID FOR DIAGNOSIS
                     var patient_array = []
                     $.ajax({
@@ -133,19 +128,18 @@ function register() {
                         success: function (data) {
                             console.log("DATA " + data)
                             patient_array = data;
+                            // Function to create diagnosis (or diagnoses).
                             createDiagnosis(patient_array.patient_id)
                             console.log("patient_array.patient_id: " + patient_array.patient_id)
                             console.log("Created diagnosis!")
                         }
                     });
 
-
-
                 }
             });
 
 
-            // Send Email
+            // Send verification email on registration.
             user.sendEmailVerification().then(function () {
                 $('#alertEmailModal').modal('toggle');
             }).catch(function (error) {
@@ -153,6 +147,7 @@ function register() {
 
             console.log(user)
         });
+        // Catch errors.
         promise.catch(e => {
             console.log(e.message);
             const registerAlert = document.getElementById("registerAlert");
@@ -171,49 +166,48 @@ function register() {
     }
 }
 
+// Diagnosis is a table on its own. Each patient can have more than 1 diagnosis.
+// This function gets all the selected diagnosis (or diagnoses) (can be one or more) and then loops through all of them. In each loop, it creates an instance
+// of the diagnosis and tags it to the patient.
+function createDiagnosis(patient_number) {
+    // This function is called in the "register()" function.
 
+    // All diagnosis select menu input have the "diagnosisClass" class.
+    allDiagnosis = $('.diagnosisClass');
 
-// var auth = firebase.auth();
-// var emailAddress = "user@example.com";
+    // Loop to loop thorugh all the selected diagnosis (or diagnoses).
+    for (i = 0; i < allDiagnosis.length; i++) {
 
+        // In each loop, create an instance of diagnosis and tag it to the patient.
+        single_diagnosis = { patient_id: patient_number, diagnosis1: allDiagnosis[i].options[allDiagnosis[i].selectedIndex].text };
 
+        console.log(single_diagnosis);
 
-// #forgetPasswordTxt
-function forgotPassword() {
-    // forgotPasswordEmail
-    var auth = firebase.auth();
-    const fpEmail = document.getElementById("forgotPasswordEmail");
-    const email = fpEmail.value;
+        // Jquery's AJAX function to POST (create) an instance to the diagnosis table.
+        $.ajax({
+            type: 'POST',
+            url: diagnosisURI,
+            data: JSON.stringify(single_diagnosis),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                console.log("Diagnosis created.");
 
-    auth.sendPasswordResetEmail(email).then(function () {
-        console.log("Email to change password sent to: " + email);
-        $('#forgotPasswordModal').modal('hide');
-    }).catch(function (error) {
-        // An error happened.
-        console.log("An error occurred.");
-        const forgotPwAlert = document.getElementById("forgotPwAlert");
-
-        forgotPwAlert.classList.remove("hide");
-        forgotPwAlert.innerHTML = error.message;
-    });
+            }
+        });
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// DIAGNOSIS
-var diagnosisCounter = 1;
+// This function called by a small plus (+) icon. When clicked, a new select menu for diagnosis is generated.
+var diagnosisCounter = 1; // Need a counter so that each diagnosis select menu have different ID (USED FOR REMOVING DIAGNOSIS).
+// NOTE: This function only ADDS a new select menu. It does not create an instance (that is done in the createDiagnosis() function).
 function addNewDiagnosis() {
+    // Each time a new diagnosis select menu is created, the counter increases by one. This is so that the select menus have different ID.
     diagnosisCounter = diagnosisCounter + 1;
     document.getElementById("diagnosisBlock").style.display = "block";
+
+    // Creation of a new diagnosis select menu.
+    // Note the ID part. The id is added with the diagnosisCounter which increments for each diagnosis menu created.
     var html = "<div id='diagnosis-" + diagnosisCounter + "'>" +
         "<select class='custom-select diagnosisClass' style='margin-bottom: 10px; width: 80%; font-size: 20px;'>" +
         "<option>De Quervain's thyroiditis</option>" +
@@ -236,59 +230,38 @@ function addNewDiagnosis() {
     $("#diagnosisBlock").append(html);
 
     console.log("New diagnosis number: " + diagnosisCounter)
-    // console.log(diagnosisCounter)
 }
-// id='removeDiagnosis'" + diagnosisCounter + "' index='" + diagnosisCounter + "'
+
+// Function to remove diagnosis select menu are that are added. *CANNOT REMOVE THE FIRST ONE*.
+// This function is called by a small (X) button beside each new diagnosis select menu.
+// Since each diagnosis select menu (except for the default one) has different ID, this function removes 
+// them by using their unique IDs.
 function removeAddedDiagnosis(counter) {
+    // Get the selected diagnosis select menu's ID.
     var diagnosisNumberX = document.getElementById("diagnosis-" + counter);
+    // Remove.
     diagnosisNumberX.remove();
     console.log("Removed Diagnosis Number: " + counter)
 }
 
-var diagnosisURI = "https://localhost:44395/api/diagnosis"
+// #forgetPasswordTxt
+// Sends change password email to the user. User can then change his/her password from their email account.
+function forgotPassword() {
+    // forgotPasswordEmail
+    var auth = firebase.auth();
+    const fpEmail = document.getElementById("forgotPasswordEmail");
+    const email = fpEmail.value;
 
-function createDiagnosis(patient_number) {
-    // var diagnosis = document.getElementById("registerDiagnosis");
-    // var diagnosisChosen = diagnosis.options[diagnosis.selectedIndex].text;
+    auth.sendPasswordResetEmail(email).then(function () {
+        console.log("Email to change password sent to: " + email);
+        $('#forgotPasswordModal').modal('hide');
+    }).catch(function (error) {
+        // Catch errors such as email does not exist.
+        // An error happened.
+        console.log("An error occurred.");
+        const forgotPwAlert = document.getElementById("forgotPwAlert");
 
-    allDiagnosis = $('.diagnosisClass');
-    diagnosis_data = [];
-    for (i = 0; i < allDiagnosis.length; i++) {
-        // console.log(allDiagnosis[i].options[allDiagnosis[i].selectedIndex].text)
-        // diagnosis_data.push(allDiagnosis[i].options[allDiagnosis[i].selectedIndex].text) // 
-        single_diagnosis = { patient_id: patient_number, diagnosis1: allDiagnosis[i].options[allDiagnosis[i].selectedIndex].text };
-        // i put 1 first to test
-
-        console.log(single_diagnosis);
-        // diagnosis_data.push(single_diagnosis); // doesnt work, not sure why.
-
-        $.ajax({
-            type: 'POST',
-            url: diagnosisURI,
-            data: JSON.stringify(single_diagnosis),
-            dataType: 'json',
-            contentType: 'application/json',
-            success: function (data) {
-                console.log("Diagnosis created.");
-
-            }
-        });
-
-        // diagnosis_data = { patient_id: 1, diagnosis1: allDiagnosis.options[allDiagnosis.selectedIndex].text }
-
-    }
-    console.log(diagnosis_data);
-
-    // $.ajax({
-    //     type: 'POST',
-    //     url: diagnosisURI,
-    //     data: JSON.stringify(diagnosis_data),
-    //     dataType: 'json',
-    //     contentType: 'application/json',
-    //     success: function (data) {
-    //         console.log("Diagnosis created.");
-
-    //     }
-    // });
-
+        forgotPwAlert.classList.remove("hide");
+        forgotPwAlert.innerHTML = error.message;
+    });
 }
